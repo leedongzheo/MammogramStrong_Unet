eefbef10ac7b021556333346914163b851a6125c
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-04-24 21:45:17 +0700

    Update train.py

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ def main():
     global trainer
     SEED=42
     torch.manual_seed(SEED)
-    model1 = unet_pyramid_cbam_gate.PyramidCbamGateUNet()
+    model1 = Unet.UNet()
     optimizer1 = optimizer.optimizer(model = model1)
     trainer = Trainer(model = model1, optimizer = optimizer1)
     if args.mode == "train":
