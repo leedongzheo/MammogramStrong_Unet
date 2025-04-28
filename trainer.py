ab1baab44805d597122b74048ba11ba8f94b2f36
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-04-28 16:06:37 +0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -116,8 +116,6 @@ class Trainer:
                     if (i + 1) % self.log_interval == 0:
                         val_loader_progress.set_postfix({'Step': i + 1, 'Loss': loss.item(), 'Dice': dice.item(), 'Iou': iou.item()})
 
-            # self.scheduler.step() #=> CosineAnnealingLR(self.optimizer, T_max=T_max, eta_min=lr_min)
-            self.scheduler.step(avg_val_loss) #=> scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3)
             
             avg_train_loss = train_loss / len(train_loader)
             avg_val_loss = val_loss / len(val_loader)
@@ -126,6 +124,9 @@ class Trainer:
             avg_train_iou = train_iou / len(train_loader)
             avg_val_iou = val_iou / len(val_loader)
 
+            # self.scheduler.step() #=> CosineAnnealingLR(self.optimizer, T_max=T_max, eta_min=lr_min)
+            self.scheduler.step(avg_val_loss) #=> scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3)
+            
             print(f"Epoch {epoch+1}: LR {self.scheduler.get_last_lr()[0]}, Train Loss {avg_train_loss:.4f}, Val Loss {avg_val_loss:.4f}, Train Dice {avg_train_dice:.4f}, Val Dice {self.avg_val_dice:.4f}, Train Iou {avg_train_iou:.4f}, Val Iou {avg_val_iou:.4f}")
             # print(f"Epoch {epoch+1}: Train Loss {avg_train_loss:.4f}, Val Loss {avg_val_loss:.4f}, Train Dice {avg_train_dice:.4f}, Val Dice {self.avg_val_dice:.4f}, Train Iou {avg_train_iou:.4f}, Val Iou {avg_val_iou:.4f}")
             self.train_losses.append(avg_train_loss)
@@ -225,8 +226,10 @@ class Trainer:
             self.avg_val_dice = val_dice / len(val_loader)
             avg_train_iou = train_iou / len(train_loader)
             avg_val_iou = val_iou / len(val_loader)
+            
             # self.scheduler.step() => CosineAnnealingLR(self.optimizer, T_max=T_max, eta_min=lr_min)
             self.scheduler.step(avg_val_loss) # => scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3)
+            
             print(f"Epoch {epoch+1}: LR {self.scheduler.get_last_lr()[0]}, Train Loss {avg_train_loss:.4f}, Val Loss {avg_val_loss:.4f}, Train Dice {avg_train_dice:.4f}, Val Dice {self.avg_val_dice:.4f}, Train Iou {avg_train_iou:.4f}, Val Iou {avg_val_iou:.4f}")
             # print(f"Epoch {epoch+1}: Train Loss {avg_train_loss:.4f}, Val Loss {avg_val_loss:.4f}, Train Dice {avg_train_dice:.4f}, Val Dice {self.avg_val_dice:.4f}, Train Iou {avg_train_iou:.4f}, Val Iou {avg_val_iou:.4f}")
             self.train_losses.append(avg_train_loss)
