684e755223d4f2a5a32f081067a16b76af7be917
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-05-05 12:03:07 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -59,11 +59,11 @@ class SegmentationDataset(Dataset):
 		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 		mask = cv2.imread(self.maskPaths[idx], 0)
 		if self.transforms:
-	            augmented = self.transforms(image=image, mask=mask)
-	            image = augmented["image"]
-	            mask = augmented["mask"].unsqueeze(0)  # (1, H, W) để phù hợp với U-Net
-		    print("shape_image: ", image.shape)
-		    print("shape_mask: ", mask.shape)
+	        	qaugmented = self.transforms(image=image, mask=mask)
+	        	image = augmented["image"]
+	        	mask = augmented["mask"].unsqueeze(0)  # (1, H, W) để phù hợp với U-Net
+			print("shape_image: ", image.shape)
+			print("shape_mask: ", mask.shape)
 		return (image, mask)
 		# check to see if we are applying any transformations
 		# if self.transforms is not None:
