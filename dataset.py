abe9483e0ffc15b704564508cb84db0f9bb107de
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2025-05-28 12:44:03 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -17,6 +17,10 @@ class SegmentationDataset(Dataset):
 		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 		mask = cv2.imread(maskPath, cv2.IMREAD_GRAYSCALE) # với cv2.IMREAD_GRAYSCALE = 0
 		if self.transforms:
+			# Debug nếu cần
+			# print(f"[DEBUG] image: {image.shape}, mask: {mask.shape}")
+			if image.shape[:2] != mask.shape[:2]:
+				raise ValueError(f"[SHAPE ERROR] Image and mask shapes do not match: {image.shape[:2]} vs {mask.shape[:2]}")
 			# grab the image path from the current index
 			augmented = self.transforms(image=image, mask=mask)
 			image = augmented["image"]
